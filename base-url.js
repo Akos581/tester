5431c9620bcb59af7576129e0a237bf1384c222c
Akos581 <akos.server.online@gmail.com>
2025-09-04 14:02:04 +0200

    Delete base-url.js

--- base-url.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  if (location.hostname.includes('github.io')) {
-    var base = document.createElement('base');
-    base.href = 'https://akos581.github.io/tester/';
-    document.head.appendChild(base);
-  }
-})();
